a42238e9e05bf806982ea6c4363da28c10a8f298
me-lucas-al <me.lucasalmeida@gmail.com>
2025-12-28 23:44:26 -0300

    feat: initialize mongodb database and docker

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerDocument } from './swagger';
 
@@ -6,6 +8,12 @@ const app = express();
 const port = 3000;
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+mongoose.connect(process.env.MONGO_URI || '').then(()=> {
+  console.log('Conectado ao MongoDB com sucesso');
+}).catch((error) => {
+  console.error('Erro ao conectar ao MongoDB:', error);
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
